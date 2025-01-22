102bb9797cf8c4e36208b9effe87fc376db65865
beginor <beginor@qq.com>
2025-01-23 06:30:53 +0800

    how it works part 2

--- src/app.ts
+++ src/app.ts
@@ -44,31 +44,53 @@ export class App {
         const program = utils.createProgramFromSources(gl, [vs, fs]);
 
         const positionLocation = gl.getAttribLocation(program, 'a_position');
+        const colorLocation = gl.getAttribLocation(program, 'a_color');
         const matrixLocation = gl.getUniformLocation(program, 'u_matrix');
 
+        // set positions;
         const vao = gl.createVertexArray();
         gl.bindVertexArray(vao);
 
-        const buffer = gl.createBuffer();
-        gl.bindBuffer(gl.ARRAY_BUFFER, buffer);
+        const positionBuffer = gl.createBuffer();
+        gl.bindBuffer(gl.ARRAY_BUFFER, positionBuffer);
 
-        this.setGeometry(gl);
+        setGeometry(gl);
 
         gl.enableVertexAttribArray(positionLocation);
 
-        const size = 2;
-        const type = gl.FLOAT;
-        const normalize = false;
-        const stride = 0;
-        const offset = 0;
+        const posSize = 2;
+        const posType = gl.FLOAT;
+        const posNormalize = false;
+        const posStride = 0;
+        const posOffset = 0;
         gl.vertexAttribPointer(
             positionLocation,
-            size,
-            type,
-            normalize,
-            stride,
-            offset
+            posSize,
+            posType,
+            posNormalize,
+            posStride,
+            posOffset
         );
+
+        // set colors
+        const colorBuffer = gl.createBuffer();
+        gl.bindBuffer(gl.ARRAY_BUFFER, colorBuffer);
+        setColors(gl);
+        gl.enableVertexAttribArray(colorLocation);
+        const colorSize = 4;
+        const colorType = gl.FLOAT;
+        const colorNormalize = false;
+        const colorStride = 0;
+        const colorOffset = 0;
+        gl.vertexAttribPointer(
+            colorLocation,
+            colorSize,
+            colorType,
+            colorNormalize,
+            colorStride,
+            colorOffset
+        );
+
 
         const translation = [200, 150];
         let angleInRadians = 0;
@@ -126,22 +148,46 @@ export class App {
             gl.uniformMatrix3fv(matrixLocation, false, matrix);
 
             const offset = 0;
-            const count = 3;
+            const count = 6;
             gl.drawArrays(gl.TRIANGLES, offset, count);
-
         }
-    }
 
-    private setGeometry(gl: WebGL2RenderingContext): void {
-        gl.bufferData(
-            gl.ARRAY_BUFFER,
-            new Float32Array([
-                0, -100,
-                150, 125,
-                -175, 100,
-            ]),
-            gl.STATIC_DRAW
-        );
+        function setGeometry(gl: WebGL2RenderingContext): void {
+            gl.bufferData(
+                gl.ARRAY_BUFFER,
+                new Float32Array([
+                    -150, -100,
+                    150, -100,
+                    -150,  100,
+                    150, -100,
+                    -150,  100,
+                    150,  100,
+                ]),
+                gl.STATIC_DRAW
+            );
+        }
+
+        function setColors(gl: WebGL2RenderingContext): void {
+            const r1 = Math.random();
+            const b1 = Math.random();
+            const g1 = Math.random();
+            const r2 = Math.random();
+            const g2 = Math.random();
+            const b2 = Math.random();
+            gl.bufferData(
+                gl.ARRAY_BUFFER,
+                new Float32Array([
+                    r1, g1, b1, 1,
+                    r1, g1, b1, 1,
+                    r1, g1, b1, 1,
+                    r2, g2, b2, 1,
+                    r2, g2, b2, 1,
+                    r2, g2, b2, 1,
+                ]),
+                gl.STATIC_DRAW
+            );
+        }
+
     }
 
 }
